4a1b5198543b39fc5f0b4a72ce5c680847067c2a
aasdsad@asd.asd <fmi@fmi.uni-plovdiv.bg>
2020-01-25 12:23:07 +0200

    Added Router in app.js

---
+++ src/pages/Users.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Users extends React.Component {
+    
+ 
+
+    render()  {
+      return <div>
+          Users Page
+      </div>
+    }
+
+  }
+  
+  export default Users;
